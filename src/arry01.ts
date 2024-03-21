c555f5413fcfed3ab12bd7c1f2d396aebf8d2dd2
Rizwan1056 <rizwi1056@gmail.com>
2024-03-21 23:07:11 +0500

    first commit program

---
+++ src/arry01.ts
@@ -0,0 +1,10 @@
+let alphabet1 = ['a','b',3,'d',5,'f'];  // in JavaScript format
+ console.log(alphabet1.length);         // 6
+ console.log(alphabet1[3]);             // d
+ 
+
+let alphabet :any [] = ['a','b',3,'d',5,'f'];  // in TypeScript Format
+console.log(alphabet.length);
+console.log(alphabet[0]);
+
+    
---
+++ src/calculator.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  {
+    message: "Enter 1st Number :",
+    name: "firstNum",
+    type: "number",
+  },
+
+  {
+    message: "Enter 2nd Number :",
+    name: "secondNum",
+    type: "number",
+  },
+  {
+    message: "Select operater :",
+    name: "operator",
+    type: "list",
+    choices: ['+', "-", "*", "/", "%"],
+  }]);
+
+if (answer.operator === "+") {
+  console.log(answer.firstNum + answer.secondNum);
+}
+ else if (answer.operator == "-") {
+  console.log(answer.firstNum - answer.secondNum);
+} else if (answer.operator === "*") {
+  console.log(answer.firstNum * answer.secondNum);
+} else if (answer.operator === "/") {
+  console.log(answer.firstNum / answer.secondNum);
+} else if (answer.operator === "%") {
+  console.log(answer.firstNum % answer.secondNum);
+}
+
+else
+console.log("Invalid Operator =>");
