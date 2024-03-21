883099267f9bffe0fd26d5f611240360008e5fb2
Rizwan1056 <rizwi1056@gmail.com>
2024-03-21 23:43:23 +0500

    update Calculator

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,35 +1,37 @@
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  {
-    message: "Enter 1st Number :",
-    name: "firstNum",
-    type: "number",
-  },
+  { message: "Enter 1st Number :", name: "firstNum", type: "number" },
+  { message: "Enter 2nd Number :", name: "secondNum", type: "number" },
 
   {
-    message: "Enter 2nd Number :",
-    name: "secondNum",
-    type: "number",
-  },
-  {
-    message: "Select operater :",
+    message: "Select operator :",
     name: "operator",
     type: "list",
-    choices: ['+', "-", "*", "/", "%"],
-  }]);
+    choices: ["+", "-", "*", "/", "%","^"],
+  },]);
 
 if (answer.operator === "+") {
+  console.log("Addition of " + answer.firstNum + " and " + answer.secondNum + " is : ");
   console.log(answer.firstNum + answer.secondNum);
-}
- else if (answer.operator == "-") {
-  console.log(answer.firstNum - answer.secondNum);
-} else if (answer.operator === "*") {
-  console.log(answer.firstNum * answer.secondNum);
-} else if (answer.operator === "/") {
-  console.log(answer.firstNum / answer.secondNum);
-} else if (answer.operator === "%") {
-  console.log(answer.firstNum % answer.secondNum);
-}
 
-else
-console.log("Invalid Operator =>");
+} else if (answer.operator == "-") {
+  console.log("Subtraction of " + answer.firstNum + " and " + answer.secondNum + " is : ");
+  console.log(answer.firstNum - answer.secondNum);
+
+} else if (answer.operator === "*") {
+  console.log("Multiplication of " +answer.firstNum +" and " +answer.secondNum +" is : ");
+  console.log(answer.firstNum * answer.secondNum);
+
+} else if (answer.operator === "/") {
+  console.log("Division of " + answer.firstNum + " and " + answer.secondNum + " is : ");
+  console.log(answer.firstNum / answer.secondNum);
+
+} else if (answer.operator === "%") {
+  console.log("Modulus of " + answer.firstNum + " and " + answer.secondNum + " is : ");
+  console.log(answer.firstNum % answer.secondNum);
+
+} else if (answer.operator === "^") {
+  console.log("Power of " + answer.firstNum + " and " + answer.secondNum + " is : ");
+  console.log(answer.firstNum ** answer.secondNum);
+
+} else console.log("Invalid Operator =>");
